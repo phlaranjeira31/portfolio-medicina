7c69fb8379f944806ea52d2c55096f2a312301e2
phlaranjeira <pedrolaranjeira09@gmail.com>
2026-05-04 08:53:02 -0300

    fix: corrige import BotaoPDF

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import BotaoPDF from "./BotaoPDF";
+import BotaoPDF from "@/app/components/BotaoPDF";
 
 const links = [
   { label: "Sobre", href: "#sobre" },
